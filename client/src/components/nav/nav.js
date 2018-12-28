fe862a30f7deb59c023dd9baba4943db930f5b13
flische <flische@gmail.com>
2018-12-28 13:20:26 -0800

    sign up component added redux form and connected redux

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -57,7 +57,7 @@ class Nav extends Component{
     }
 
     renderLinks = () => {
-        const auth = true;
+        const auth = false;
         let authLinks = [];
 
         const { auth: navAuth, common, nonAuth} = this.state;
