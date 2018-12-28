0441104bc872dc90b6a98e520eea33565b3b22e8
flische <flische@gmail.com>
2018-12-28 11:41:29 -0800

    worked on nav component with user auth

--- client/src/components/nav/nav.js
+++ client/src/components/nav/nav.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import './nav.scss';
 
 class Nav extends Component{
 
@@ -37,9 +39,50 @@ class Nav extends Component{
         ]
     }
 
+    renderSignOut(){
+        return (
+            <button onClick={ () => (console.log('Sign Out clicked!'))} 
+                    className="btn waves-effect waves-light blue">Sign Out
+            </button>
+        );
+    }
+
+    buildLink(link){
+        return (
+            <li key={link.to}>
+                <Link to={link.to}>{link.text}</Link>
+            </li>
+        );
+    }
+
+    renderLinks(){
+        const auth = true;
+        let authLinks = [];
+
+        const { auth: navAuth, common, nonAuth} = this.state;
+
+        const commonLinks = common.map(this.buildLink);
+
+        if(auth){
+            authLinks = navAuth.map(this.buildLink);
+            authLinks.push( <li key='/sign-out'> { this.renderSignOut() } </li> );
+        } else {
+            authLinks = nonAuth.map(this.buildLink);
+        }
+
+        return [ ...commonLinks, ...authLinks ];
+    }
+
     render(){
         return (
-            <nav className="blue-grey darken-2"></nav>
+            <nav className="blue-grey darken-2">
+                <div className="nav-wrapper">
+                    <Link className="brand-logo" to="/">iShop</Link>
+                    <ul className="right">
+                        {this.renderLinks()}
+                    </ul>
+                </div>
+            </nav>
         );
     }
 }
