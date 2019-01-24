4f01d8d9b3d51b1362a10af5f1677346d0560a9a
flische <flische@gmail.com>
2019-01-24 11:40:33 -0800

    completed setting up signing up and signing out via redux store

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,9 +4,12 @@ import axios from 'axios';
 
 export const jwtSignIn = () => async dispatch => {
     try {
-        const resp = await axios.get('/auth/jwt-sign-in', authHeaders());
+        const { data: { user} } = await axios.get('/auth/jwt-sign-in', authHeaders());
 
-        console.log('JWT Sign In response:', resp);
+        dispatch({
+            type: types.SIGN_IN,
+            user
+        });
 
     } catch(err){
         console.log('Error with JWT Sign In:', err);
@@ -15,11 +18,14 @@ export const jwtSignIn = () => async dispatch => {
 
 export const signIn = userInfo => async dispatch => {
     try {
-        const resp = await axios.post('/auth/sign-in', userInfo);
+        const { data: { token, user } } = await axios.post('/auth/sign-in', userInfo);
 
-        console.log('Sign In response: ', resp);
+        localStorage.setItem('token',token);
 
-        localStorage.setItem('token', resp.data.token);
+        dispatch({
+            type: types.SIGN_IN,
+            user
+        });
 
     } catch(err){
         console.log('Error Signing In: ', err);
@@ -28,11 +34,22 @@ export const signIn = userInfo => async dispatch => {
 
 export const signUp = userInfo => async dispatch => {
     try {
-        const resp = await axios.post('/auth/sign-up', userInfo);
+        const { user: { token, user } } = await axios.post('/auth/sign-up', userInfo);
 
-        console.log('Sign Up response: ', resp);
+        localStorage.setItem('token',token);
+
+        dispatch({
+            type: types.SIGN_UP,
+            user
+        });
 
     } catch(err){
         console.log('Sign Up error: ', err.message);
     }
+}
+
+export const signOut = () => {
+    localStorage.removeItem('token');
+
+    return { type: types.SIGN_OUT };
 }
